407fe3d826ae33b16eb4a72613966decd9bcf380
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-12-20 18:20:22 +0600

    accessories api added

--- controllers/accessariesController.js
+++ controllers/accessariesController.js
@@ -20,5 +20,33 @@ const  getAccessories = async (req, res) => {
     }
 }
 
+const getAccessoriesById = async(req,res)=>{
+    try {
+        const accessorie = await Accessories.findById(req.params.id);
+        if (!accessorie) {
+            return res.status(404).json({ msg: 'Package not found' });
+        }
+        res.json(accessorie);
+    } catch (error) {
+        
+        console.error(error);
+        res.status(500).json({ msg: 'Error fetching package', error: error.message });
+    }
+}
+const getAccessoriesByCategory = async (req, res) => {
+    try {
+        const accessories = await Accessories.find({ category: req.params.category });
 
-module.exports = { createAccessories, getAccessories };
+        if (!accessories || accessories.length === 0) {
+            return res.status(404).json({ msg: 'No accessories found for this category' });
+        }
+
+        res.json(accessories);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Error fetching accessories', error: error.message });
+    }
+};
+
+
+module.exports = { createAccessories, getAccessories, getAccessoriesById, getAccessoriesByCategory };
--- routes/accessoriesRoutes.js
+++ routes/accessoriesRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createAccessories, getAccessories } = require('../controllers/accessariesController');
+const { createAccessories, getAccessories, getAccessoriesById, getAccessoriesByCategory } = require('../controllers/accessariesController');
 
 const router = express.Router();
 
 router.post('/', createAccessories);
 router.get('/', getAccessories);
+router.get('/:id', getAccessoriesById);
+router.get('/:id/:category', getAccessoriesByCategory);
 
 module.exports = router;
